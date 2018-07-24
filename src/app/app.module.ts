50ae2b6d2b23fd4be16437f7e07ce4cd08478fab
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-07-24 10:43:01 -0600

    Declaración de componentes y routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ShowNotesComponent } from './show-notes/show-notes.component';
+import { CreateNoteComponent } from './create-note/create-note.component';
+import { RouterModule } from "@angular/router";
+import { ROUTES } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowNotesComponent,
+    CreateNoteComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
